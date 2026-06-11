9be7551025eb6bbd9ef1951631c49689621c4207
zmdhh <zmdhh@gmail.com>
2026-06-11 19:10:50 +0800

    Add close button to H5 in-app player

--- pages/play-h5/play-h5.vue
+++ pages/play-h5/play-h5.vue
@@ -1,5 +1,6 @@
 <template>
   <view class="container" v-if="url">
+    <view class="close-btn" @tap="goBack">✕</view>
     <!-- #ifdef APP-PLUS -->
     <web-view :src="url" @message="onMsg"></web-view>
     <!-- #endif -->
@@ -18,11 +19,9 @@ export default {
     const m3u8 = options.url || ''
     const title = options.title || ''
     const poster = options.poster || ''
-    
     const params = '?url=' + encodeURIComponent(m3u8) +
       '&title=' + encodeURIComponent(title) +
       '&poster=' + encodeURIComponent(poster)
-    
     // #ifdef APP-PLUS
     this.url = 'https://zmdhh.github.io/cms-video-app/h5-player/' + params
     // #endif
@@ -31,15 +30,13 @@ export default {
     // #endif
   },
   methods: {
-    onMsg(e) {
-      if (e.detail && e.detail.data === 'close') {
-        uni.navigateBack()
-      }
-    }
+    goBack() { uni.navigateBack() },
+    onMsg(e) {}
   }
 }
 </script>
 
 <style scoped>
-.container{width:100%;height:100vh}
+.container{width:100%;height:100vh;position:relative}
+.close-btn{position:fixed;top:20rpx;right:20rpx;z-index:999;width:56rpx;height:56rpx;background:rgba(0,0,0,0.6);color:#fff;border-radius:50%;display:flex;align-items:center;justify-content:center;font-size:28rpx}
 </style>
