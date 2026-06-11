b916f92145ac709843b49ed1e33114024d9276ac
zmdhh <zmdhh@gmail.com>
2026-06-11 20:26:38 +0800

    Force landscape on H5 player page load

--- pages/play-h5/play-h5.vue
+++ pages/play-h5/play-h5.vue
@@ -1,12 +1,15 @@
 <template>
-  <web-view v-if="url" :src="url" @message="onMsg" style="width:100%;height:100vh"></web-view>
+  <view class="container" v-if="url">
+    <web-view :src="url" style="width:100%;height:100%"></web-view>
+    <view class="rotate-btn" @tap="toggleOrient">{{ isLand ? '↻竖屏' : '↻横屏' }}</view>
+  </view>
   <view v-else class="loading"><text>加载中...</text></view>
 </template>
 
 <script>
 export default {
   data() {
-    return { url: '' }
+    return { url: '', isLand: true }
   },
   onLoad() {
     const raw = uni.getStorageSync('h5_player_data')
@@ -21,27 +24,28 @@ export default {
         this.url = '/static/player/index.html' + params + epsStr
       } catch(e) {}
     }
-  },
-  methods: {
-    onMsg(e) {
-      if (e.detail && e.detail.data) {
-        var d = e.detail.data
-        if (d.close) uni.navigateBack()
-        // #ifdef APP-PLUS
-        if (d.landscape) plus.screen.lockOrientation('landscape')
-        if (d.portrait) plus.screen.lockOrientation('portrait')
-        // #endif
-      }
-    }
+    // #ifdef APP-PLUS
+    plus.screen.lockOrientation('landscape')
+    // #endif
   },
   onUnload() {
     // #ifdef APP-PLUS
     plus.screen.lockOrientation('portrait')
     // #endif
+  },
+  methods: {
+    toggleOrient() {
+      // #ifdef APP-PLUS
+      this.isLand = !this.isLand
+      plus.screen.lockOrientation(this.isLand ? 'landscape' : 'portrait')
+      // #endif
+    }
   }
 }
 </script>
 
 <style scoped>
+.container{width:100%;height:100vh;position:relative}
 .loading{display:flex;justify-content:center;padding-top:200rpx;color:#888;font-size:28rpx}
+.rotate-btn{position:fixed;bottom:30rpx;right:30rpx;z-index:9999;padding:10rpx 20rpx;background:rgba(34,197,94,0.8);color:#fff;border-radius:50rpx;font-size:22rpx}
 </style>
