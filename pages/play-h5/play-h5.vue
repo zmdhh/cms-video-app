964ad85e7cdd0ea4e2c986c6b7554d6c6bda8dcb
zmdhh <zmdhh@gmail.com>
2026-06-12 15:39:03 +0800

    Fix black screen: clear stale data, add cache buster, robust cleanup

--- pages/play-h5/play-h5.vue
+++ pages/play-h5/play-h5.vue
@@ -15,30 +15,34 @@ export default {
     }, 300)
     // #endif
     const raw = uni.getStorageSync('h5_player_data')
+    uni.removeStorageSync('h5_player_data')
     if (raw) {
       try {
         const data = JSON.parse(raw)
         const eps = data.urls && data.titles ? { u: data.urls, t: data.titles } : null
         const params = '?url=' + encodeURIComponent(data.url || '') +
           '&title=' + encodeURIComponent(data.title || '') +
-          '&poster=' + encodeURIComponent(data.poster || '')
+          '&poster=' + encodeURIComponent(data.poster || '') +
+          '&_t=' + Date.now()
         const epsStr = eps ? '&eps=' + encodeURIComponent(JSON.stringify(eps)) : ''
         this.url = '/static/player/index.html' + params + epsStr
       } catch(e) {}
     }
     var self = this
+    plus.storage.removeItem('__exit_player__')
     this._checkTimer = setInterval(function() {
       try {
         if (plus.storage.getItem('__exit_player__') === '1') {
           plus.storage.removeItem('__exit_player__')
           clearInterval(self._checkTimer)
+          self._checkTimer = null
           uni.navigateBack()
         }
       } catch(e) {}
     }, 300)
   },
   onUnload() {
-    if (this._checkTimer) clearInterval(this._checkTimer)
+    if (this._checkTimer) { clearInterval(this._checkTimer); this._checkTimer = null }
     try { plus.storage.removeItem('__exit_player__') } catch(e) {}
     // #ifdef APP-PLUS
     plus.screen.lockOrientation('portrait')
