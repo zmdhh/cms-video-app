98f9ac0d8768a320e32b101abb74902768640ac3
zmdhh <zmdhh@gmail.com>
2026-06-12 15:19:40 +0800

    Move back button into webview toolbar, use close(auto)

--- pages/play-h5/play-h5.vue
+++ pages/play-h5/play-h5.vue
@@ -1,9 +1,5 @@
 <template>
-  <view class="container" v-if="url">
-    <web-view :src="url" style="width:100%;height:100%"></web-view>
-    <view class="close-btn left" @tap="goBack">‹ 返回</view>
-    <view class="close-btn right" @tap="goBack">✕</view>
-  </view>
+  <web-view v-if="url" :src="url" style="width:100%;height:100vh"></web-view>
   <view v-else class="loading"><text>加载中...</text></view>
 </template>
 
@@ -48,9 +44,5 @@ export default {
 </script>
 
 <style scoped>
-.container{width:100%;height:100vh;position:relative}
 .loading{display:flex;justify-content:center;padding-top:200rpx;color:#888;font-size:28rpx}
-.close-btn{position:fixed;z-index:99999;background:rgba(0,0,0,0.6);color:#fff;border-radius:8rpx;display:flex;align-items:center;justify-content:center;font-size:26rpx;padding:8rpx 20rpx}
-.close-btn.left{top:10rpx;left:10rpx}
-.close-btn.right{top:10rpx;right:10rpx;padding:8rpx 16rpx;font-size:30rpx}
 </style>
