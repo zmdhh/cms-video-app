30fda91bade00601becc342ab33f12ecdd23abe0
zmdhh <zmdhh@gmail.com>
2026-06-12 15:17:13 +0800

    Add back button at top-left, remove broken ArtPlayer exit

--- pages/play-h5/play-h5.vue
+++ pages/play-h5/play-h5.vue
@@ -1,7 +1,8 @@
 <template>
   <view class="container" v-if="url">
     <web-view :src="url" style="width:100%;height:100%"></web-view>
-    <view class="close-btn" @tap="goBack">✕</view>
+    <view class="close-btn left" @tap="goBack">‹ 返回</view>
+    <view class="close-btn right" @tap="goBack">✕</view>
   </view>
   <view v-else class="loading"><text>加载中...</text></view>
 </template>
@@ -49,5 +50,7 @@ export default {
 <style scoped>
 .container{width:100%;height:100vh;position:relative}
 .loading{display:flex;justify-content:center;padding-top:200rpx;color:#888;font-size:28rpx}
-.close-btn{position:fixed;top:10rpx;right:10rpx;z-index:99999;width:60rpx;height:60rpx;background:rgba(0,0,0,0.7);color:#fff;border-radius:50%;display:flex;align-items:center;justify-content:center;font-size:30rpx}
+.close-btn{position:fixed;z-index:99999;background:rgba(0,0,0,0.6);color:#fff;border-radius:8rpx;display:flex;align-items:center;justify-content:center;font-size:26rpx;padding:8rpx 20rpx}
+.close-btn.left{top:10rpx;left:10rpx}
+.close-btn.right{top:10rpx;right:10rpx;padding:8rpx 16rpx;font-size:30rpx}
 </style>
